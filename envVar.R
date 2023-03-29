c6c5bd2340442315febf3638f189ade4506b3781
Dhiraj Upadhyaya <dupadhyaya@users.noreply.github.com>
2023-03-29 17:47:44 +0530

    a

---
+++ envVar.R
@@ -0,0 +1,12 @@
+#configurations
+
+path1 <- Sys.getenv('PATH')
+
+Sys.setenv(PATH = paste("C:\\rtools43\\bin", Sys.getenv("PATH"), sep=";"))
+
+
+Sys.which('make')
+
+paste(path1, collapse = ';')
+
+strsplit(path1, split =';')
